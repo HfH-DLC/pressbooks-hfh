dfef74e410334f4971b8334ad2ba491ea4abfa91
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-08-10 12:51:25 +0200

    Update book_posts_password.php

--- components/book_posts_password.php
+++ components/book_posts_password.php
@@ -27,17 +27,17 @@ class BookPostsPassword
     private function __construct()
     {
         add_action('admin_menu', array($this, 'add_settings_page'));
-        add_action('admin_post_set_book_posts_password', array($this, 'set_book_posts_password'));
+        add_action('admin_post_set_book_password', array($this, 'set_book_password'));
     }
 
     public function add_settings_page()
     {
         add_submenu_page(
             'pb_organize',
-            __('Book Posts Password', 'pressbooks-hfh'),
-            __('Book Posts Password', 'pressbooks-hfh'),
+            __('Book Password', 'pressbooks-hfh'),
+            __('Book Password', 'pressbooks-hfh'),
             'edit_posts',
-            'passwords',
+            'hfh-book-password',
             array($this, 'settings_page_html'),
             20
         );
@@ -45,8 +45,8 @@ class BookPostsPassword
 
     public function settings_page_html()
     {
-        $password_set = get_transient('pressbooks-hfh-book-posts-password-set');
-        delete_transient('pressbooks-hfh-book-posts-password-set');
+        $password_set = get_transient('pressbooks-hfh-book-password-set');
+        delete_transient('pressbooks-hfh-book-password-set');
 ?>
         <div class="wrap">
             <?php if ($password_set) : ?>
@@ -56,10 +56,12 @@ class BookPostsPassword
             <?php endif; ?>
             <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
             <form action="<?= admin_url('admin-post.php') ?>" method="post">
-                <?php wp_nonce_field('pressbooks-hfh_set_book_posts_password', 'pressbooks-hfh_sbook-posts-password-nonce'); ?>
-                <input type="hidden" name="action" value="set_book_posts_password">
-                <label for="posts_password"><?php _e('Password for all Book Posts', 'pressbooks-hfh'); ?></label><br />
-                <input type="text" name="posts_password" id="posts_password" style="text-align:left" value="" placeholder="<?php esc_attr_e('Password...', 'pressbooks'); ?>" maxlength="255" />
+                <?php wp_nonce_field('pressbooks-hfh_set_book_password', 'pressbooks-hfh_book-password-nonce'); ?>
+                <input type="hidden" name="action" value="set_book_password">
+                <label for="book_password"><?php _e('Password', 'pressbooks-hfh'); ?></label>
+                <div>
+                    <input type="text" name="book_password" id="book_password" style="text-align:left" value="" placeholder="<?php esc_attr_e('Password...', 'pressbooks'); ?>" maxlength="255" />
+                </div>
                 <?php
                 submit_button(__('Save', 'pressbooks-hfh'));
                 ?>
@@ -68,11 +70,11 @@ class BookPostsPassword
 <?php
     }
 
-    public function set_book_posts_password()
+    public function set_book_password()
     {
-        check_admin_referer('pressbooks-hfh_set_book_posts_password', 'pressbooks-hfh_sbook-posts-password-nonce');
-        if (isset($_POST['posts_password'])) {
-            $post_password = $_POST['posts_password'];
+        check_admin_referer('pressbooks-hfh_set_book_password', 'pressbooks-hfh_book-password-nonce');
+        if (isset($_POST['book_password'])) {
+            $book_password = $_POST['book_password'];
             $posts = get_posts([
                 'post_type' => array('part', 'chapter', 'front-matter', 'back-matter'),
                 'post_status' => 'any',
@@ -84,12 +86,12 @@ class BookPostsPassword
                 wp_update_post(
                     [
                         'ID' => $post_id,
-                        'post_password' => $post_password,
+                        'post_password' => $book_password,
                     ]
                 );
             }
         }
-        set_transient('pressbooks-hfh-book-posts-password-set', true, MINUTE_IN_SECONDS);
+        set_transient('pressbooks-hfh-book-password-set', true, MINUTE_IN_SECONDS);
         $url = $_POST['_wp_http_referer'];
         wp_safe_redirect($url);
         exit();
