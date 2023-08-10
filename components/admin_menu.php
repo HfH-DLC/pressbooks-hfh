d63056185a8a1c1fc7d0dada6017f0824459820a
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-08-10 12:51:07 +0200

    Update admin_menu.php

--- components/admin_menu.php
+++ components/admin_menu.php
@@ -3,11 +3,12 @@
 namespace Hfh\Pressbooks;
 
 use HfH\Pressbooks\CourseProgress;
+use HfH\Pressbooks\GamipressNotificationsH5PFix;
 
 class AdminMenu
 {
     const NONCE = '_hfh_pressbooks_wpnonce';
-    const SLUG = 'hfh-pressbooks';
+    const SLUG = 'hfh';
     const ACTION = 'hfh_pressbooks_save_settings';
 
     private static $instance = false;
@@ -30,32 +31,72 @@ class AdminMenu
 
     public function add_menu()
     {
-        if (!menu_page_url('hfh', false)) {
-            add_menu_page("HfH", 'HfH', 'manage_options', 'hfh', array($this, 'display_menu'), 'dashicons-admin-generic', 26);
-        }
-        add_submenu_page('hfh', 'Pressbooks', 'Pressbooks', 'manage_options', self::SLUG, array($this, 'display_pressbooks_menu'));
+        add_menu_page("HfH Pressbooks", 'HfH Pressbooks', 'manage_options', self::SLUG);
+        add_submenu_page('hfh', 'Übersicht', 'Übersicht', 'manage_options', self::SLUG, array($this, 'display_overview'));
+        add_submenu_page('hfh', 'Einstellungen', 'Einstellungen', 'manage_options', self::SLUG . '-settings', array($this, 'display_settings'));
         add_option(CourseProgress::ACTIVE_OPTION, false);
+        add_option(GamipressNotificationsH5PFix::ACTIVE_OPTION, false);
     }
 
-    public function display_menu()
-    {
-    }
-
-    public function display_pressbooks_menu()
+    function display_overview()
     {
 ?>
-
         <div class="wrap">
-            <h2>Pressbooks</h2>
+            <h2>Übersicht</h2>
+            <h3>Buchpasswort</h3>
+            <p>Unter <a href="<?= admin_url('admin.php?page=hfh-book-password') ?>">Organisieren > Buchpasswort</a> kann ein Passwort für das komplette Buch gesetzt werden.</p>
 
+            <h3>Lesefortschritt</h3>
+            <p>
+                Unter <a href="<?= admin_url('admin.php?page=hfh-settings') ?>">HfH Pressbooks > Einstellungen</a> kann der Lesefortschritt eingestellt werden. Am Ende jedes Kapitels wird ein Button angezeigt, mit dem der Benutzer das Kapitel als bearbeitet markieren kann. Im Inhaltsverzeichnis wird angezeigt, welche Kapitel bereits bearbeitet wurden. Im Menü wird die Seite "Fortschritt" verfügbar.
+            </p>
+            <h3>Kapitelkategorien</h3>
+            <p>
+                Unter <a href="<?= admin_url('edit-tags.php?taxonomy=category&post_type=chapter') ?>">Organisieren > Kapitelkategorien</a> ist die Kategorie-Seite für Kapitel verfügbar, die in Pressbooks standardmässig versteckt ist.
+            </p>
+            <h3>Shibboleth Zugriff</h3>
+            <p>
+                Unter <a href="<?= admin_url('options-general.php?page=pressbooks_sharingandprivacy_options') ?>">Einstellungen > Freigabe & Datenschutz</a>
+                kann eingestellt werden, Angehörige welcher Organisation ein privates Buch lesen dürfen. Benutzer*innen, die über diese Option Zugriff auf ein Buch erhalten, erhalten die Rolle "Abonnent".
+                Sie verlieren die Zugriffsrechte nicht mehr, auch wenn sich diese Einstellung oder ihre Organisationsangehörigkeit ändern.
+            </p>
+            <h3>Shortcodes</h3>
+            <h4>[hfh_parts] und [hfh_chapters]</h4>
+            <p>Diese beiden Shortcodes zeigen die Teile resp. Kapitel des Buches an. Mit dem Attribut "color" kann die Hauptfarbe gesetzt werden, z.B. [hfh_chapters color="#E31826"].</p>
+            <h4>[hfh_confetti]</h4>
+            <p>Dieser Shortcode zeigt beim Laden der Seite Konfetti an. Die Farben der Konfetti können mit dem Attribute "colors" definiert werden, z.B. [hfh_confetti colors="#BE1925,#E31826,#D8757C,#F2D1D3,#F8E8E9,#FBF3F4,#767676,#14776c,#F08100,#FFCD00"].
+            </p>
+            <h3>Gamipress Notifications H5P Fix</h3>
+            <p>
+                Das Gamipress Notifications Plugin erkennt nicht, wenn ein H5P beantwortet wird, bis die Seite neu geladen wird.
+                Unter <a href="<?= admin_url('admin.php?page=hfh-settings') ?>">HfH Pressbooks > Einstellungen</a> kann ein Fix dafür aktiviert werden.
+            </p>
+        </div>
+    <?
+    }
+
+    public function display_settings()
+    {
+    ?>
+        <div class="wrap">
+            <h2>Einstellungen</h2>
             <form method="post" action="edit.php?action=<?= self::ACTION ?>">
                 <?php wp_nonce_field(self::ACTION, self::NONCE); ?>
-                <h2>Learning Progress</h2>
+                <h3>Lesefortschritt</h3>
                 <table class="form-table">
                     <tr>
-                        <th scope="row"><label for="progress_active">Show learning progress</label></th>
+                        <th scope="row"><label for="progress_active">Lesefortschritt anzeigen</label></th>
                         <td>
                             <input type="checkbox" name="<?= CourseProgress::ACTIVE_OPTION ?>" id="progress_active" value="true" <?= get_option(CourseProgress::ACTIVE_OPTION) ? 'checked' : '' ?>>
+                        </td>
+                    </tr>
+                </table>
+                <h3>Gamipress Notifications H5P Fix</h3>
+                <table class="form-table">
+                    <tr>
+                        <th scope="row"><label for="gamipress_notfications_h5p_fix_active">Fix aktivieren</label></th>
+                        <td>
+                            <input type="checkbox" name="<?= GamipressNotificationsH5PFix::ACTIVE_OPTION ?>" id="gamipress_notfications_h5p_fix_active" value="true" <?= get_option(GamipressNotificationsH5PFix::ACTIVE_OPTION) ? 'checked' : '' ?>>
                         </td>
                     </tr>
                 </table>
@@ -71,6 +112,7 @@ class AdminMenu
     {
         check_admin_referer(self::ACTION, self::NONCE);
         update_option(CourseProgress::ACTIVE_OPTION, isset($_POST[CourseProgress::ACTIVE_OPTION]));
+        update_option(GamipressNotificationsH5PFix::ACTIVE_OPTION, isset($_POST[GamipressNotificationsH5PFix::ACTIVE_OPTION]));
         $url = add_query_arg(
             array(
                 'page' => self::SLUG,
